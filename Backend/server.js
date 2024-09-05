97d1c08fd77a08c14faacc720404eedfeb3d49cc
tatabharathyadav <tatabharathyadav2003@gmail.com>
2024-09-05 18:20:16 +0530

    updating using mongo

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,9 @@
  app.use(express.json());
  
  // MongoDB connection setup
- const mongoURI = 'mongodb://localhost:27017/carehaven'; 
- mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+ const mongoURI = process.env.MONGODB_URI;
+ mongoose.connect(mongoURI
+  , { useNewUrlParser: true, useUnifiedTopology: true })
    .then(() => console.log('MongoDB connected'))
    .catch(err => console.error('MongoDB connection error:', err));
  
